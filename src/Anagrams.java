629b4ab788a32ed732b8f40f1537aa16cb82549c
YFCodeDream <52846930+YFCodeDream@users.noreply.github.com>
2022-05-21 12:07:22 +0800

    Initial commit

---
+++ src/Anagrams.java
@@ -0,0 +1,104 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author YFCodeDream
+ * @version 1.0.0
+ * @date 2022/5/14
+ * @description Anagrams
+ */
+public class Anagrams {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the anagram finder!\n");
+
+        if (args.length != 1) {
+            System.out.println("  usage: java Anagram some-text.txt\n");
+            System.exit(0);
+        }
+
+        System.out.println("  reading file " + args[0] + "...");
+
+        WordReader wr = new WordReader(args[0]);
+
+        HashMap<String, LinkedList<String>> anagramsMap = getAnagramsMap(wr);
+        printAnagramsMap(anagramsMap);
+    }
+
+    /**
+     * 获取anagram的HashMap
+     * @param wordReader 取单词类
+     * @return anagram的HashMap
+     */
+    private static HashMap<String, LinkedList<String>> getAnagramsMap(WordReader wordReader) {
+        HashMap<String, LinkedList<String>> anagramsMap = new HashMap<>();
+
+        String currentWord;
+        while ((currentWord = wordReader.nextWord()) != null) {
+            AtomicReference<Boolean> hasInserted = new AtomicReference<>(false);
+            String finalCurrentWord = currentWord;
+
+            anagramsMap.forEach((keyWord, anagramsList) -> {
+                if (keyWord.equals("care") && finalCurrentWord.equals("race"))
+                    System.out.println(checkKeyWordAnagram(finalCurrentWord, keyWord));
+                if (checkKeyWordAnagram(finalCurrentWord, keyWord)) {
+                    if (!anagramsList.contains(finalCurrentWord)) {
+                        anagramsList.add(finalCurrentWord);
+                        hasInserted.set(true);
+                    }
+                }
+            });
+
+            if (!hasInserted.get()) {
+                if (!anagramsMap.containsKey(currentWord)) {
+                    anagramsMap.put(currentWord, new LinkedList<>());
+                }
+            }
+        }
+
+        return anagramsMap;
+    }
+
+    /**
+     * 检查与当前的keyWord是不是互为anagram
+     * @param currentWord 当前单词
+     * @param keyWord 当前的keyWord
+     * @return true if 互为anagram else false
+     */
+    private static boolean checkKeyWordAnagram(String currentWord, String keyWord) {
+        return getWordCharFreqMap(currentWord).equals(getWordCharFreqMap(keyWord))
+                && !currentWord.equals(keyWord);
+    }
+
+    /**
+     * 获取一个单词字符频率
+     * @param word 当前单词
+     * @return 字符频率
+     */
+    private static HashMap<Character, Integer> getWordCharFreqMap(String word) {
+        HashMap<Character, Integer> wordCharMap = new HashMap<>();
+        for (Character currentWordChar : word.toCharArray()) {
+            Integer currentWordCharFreq = wordCharMap.get(currentWordChar);
+            currentWordCharFreq = currentWordCharFreq == null ? 0 : currentWordCharFreq;
+            wordCharMap.put(currentWordChar, currentWordCharFreq + 1);
+        }
+        return wordCharMap;
+    }
+
+    /**
+     * 打印anagram
+     * @param anagramsMap anagram的HashMap
+     */
+    private static void printAnagramsMap(HashMap<String, LinkedList<String>> anagramsMap) {
+        System.out.println("Found anagrams:");
+        anagramsMap.forEach((keyWord, anagramsList) -> {
+            StringBuilder resStr = new StringBuilder("-\t" + keyWord + "\t");
+            if (!anagramsList.isEmpty()) {
+                for (String anagram : anagramsList) {
+                    resStr.append(anagram).append("\t");
+                }
+                System.out.println(resStr);
+            }
+        });
+    }
+}
